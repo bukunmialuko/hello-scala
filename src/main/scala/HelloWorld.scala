52bb929023d601995dab5e9590f1aa0a5fadd0ad
bukunmialuko <oluwabukunmi.aluko@gmail.com>
2021-06-28 23:25:36 +0100

    If else statement

--- src/main/scala/HelloWorld.scala
+++ src/main/scala/HelloWorld.scala
@@ -1,8 +1,33 @@
 object HelloWorld extends App {
-  def sayHello(string: String) :Unit ={
-    print("Hello " + string)
+  def sayHello(string: String): Unit = {
+    println("Hello " + string)
   }
+
   sayHello("Ejiro")
 
   val x: Int = 5
+
+  val a = 3
+  val b = 5
+  if (a > b) {
+    println("a > b")
+  } else {
+    println("a < b")
+  }
+
+  //  val day = "MONi"
+  //  val week: Unit = day match {
+  //    case "MON" | "TUE" => println("Weekday")
+  //    case "SAT"  => println("Weekend")
+  //    case _ => println("Undefined")
+  //  }
+
+  // pattern matching
+  val leave = 25
+  val week = leave match {
+    case s if s <= 15 => println("Paid Leave")
+    case s if s >= 15 => println("casual Leave")
+    case s => println("Not Applicable")
+  }
 }
+
